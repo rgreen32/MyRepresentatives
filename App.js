fe812d12562ab17fce5321ac4a09d5dbc8c7cdf2
rgreen32 <ray.greenm@gmail.com>
2018-11-12 21:29:38 -0600

    Rey

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import DisplayRepInfo from './DisplayRepInfo';
+import './rep.css';
+import 'tachyons'
+
+export default class App extends Component {
+  constructor(props){
+      super(props);
+      this.state ={info:[-1], address:""}
+  }
+
+  getRepInfo = (event) =>{
+      var params = {address:this.state.address}
+      var url = new URL("https://localhost:5001/api/Data/Representatives")
+
+      url.search = new URLSearchParams(params)
+
+      fetch(url,{headers:{
+          'Content-Type': 'application/json'
+      }}).then(data=>data.json())
+      .then(response =>this.setState({info:response}))
+
+      event.preventDefault();
+      event.target.reset();
+  }
+  updateAddress = (event) =>{
+      this.setState({address: event.target.value})
+  }
+
+  render(){
+      return(
+          
+          <div align="center" class="">
+            <div id="text"><p>Welcome to MyRepresentatives! 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p>
+                </div>
+              <form id="input" onSubmit={this.getRepInfo}>
+                  <input type="text" placeholder="address" name="address" onChange={this.updateAddress}></input>
+              </form>
+              
+              <DisplayRepInfo officialInfo={this.state.info}/>
+              
+          </div>
+      )
+  }
+}
---
+++ DisplayRepInfo.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import './rep.css';
+import 'tachyons';
+// import 'bootstrap';
+
+
+class DisplayRepInfo extends Component{
+    render(){
+        if(this.props.officialInfo[0] !== -1){
+        
+        
+         var htmlBlocks = this.props.officialInfo.officials.map(
+             official =>{
+                 let img;
+                 if(!official.photoUrl){
+                    img = "ghost_person_200x200_v1.png"
+                 }else{
+                     img = official.photoUrl
+                 }
+                 
+                 return(
+                     
+                     <div class="">
+                         <img src={img}></img>
+                         <h1 class="">{official.name}</h1>
+                         <h3>Party: {official.party}</h3>
+                         <h3>Phone Number: {official.phones}</h3>
+                         <a href={official.urls}>{official.urls}</a>
+                     </div>
+                 )
+             }
+         )
+         return(
+             <div>{htmlBlocks}</div>
+        )
+         }else{
+             return(<h1></h1>)
+         }
+        
+    }
+
+}
+
+export default DisplayRepInfo;
