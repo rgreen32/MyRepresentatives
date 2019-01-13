28f368b18074f8a9a5021d244111895850178e1f
rgreen32 <ray.greenm@gmail.com>
2019-01-13 12:41:11 -0600

    added input fields

--- src/MyRepresentatives.Web/ClientApp/src/DisplayRepInfo.js
+++ src/MyRepresentatives.Web/ClientApp/src/DisplayRepInfo.js
@@ -10,18 +10,11 @@ class DisplayRepInfo extends Component{
         
          var htmlBlocks = this.props.officialInfo.officials.map(
              official =>{
-                 let img;
-                 if(official.photoUrl=== undefined){
-                    img = "no-image.png"
-                 }else{
-                     console.log(img)
-                     img = official.photoUrl
-                 }
                  
                  return(
                      
                      <div class="tc">
-                         <img class="h5 w5"src={img}></img>
+                         
                          <h1 class="">{official.name}</h1>
                          <h3>Party: {official.party}</h3>
                          <h3>Phone Number: {official.phones}</h3>
